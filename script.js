aaf4e081ea52dc73668e498db41b69ba4b017b80
sameer <thakursameer5893@gmail.com>
2026-01-31 15:21:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,1 @@
-// for reset password
-  if (window.netlifyIdentity) {
-    window.netlifyIdentity.on("init", user => {
-      if (!user) {
-        window.netlifyIdentity.on("login", () => {
-          document.location.href = "/admin/";
-        });
-      }
-    });
-  }
 
