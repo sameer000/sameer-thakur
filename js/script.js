ba19deca936905f2b90be21f7017a60b83e93fb2
sameer <thakursameer5893@gmail.com>
2026-01-31 23:47:36 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -9,3 +9,21 @@ fetch('/components/footer.html')
 .then(data => {
 document.getElementById('footer').innerHTML = data
 })
+
+// current time
+function updateIndiaTime() {
+const timeEl = document.querySelector('.current-time');
+const now = new Date();
+const options = {
+timeZone: 'Asia/Kolkata',
+hour: '2-digit',
+minute: '2-digit',
+second: '2-digit',
+hour12: true
+};
+let time = now.toLocaleTimeString('en-IN', options)
+.replace(' ', '');
+timeEl.innerHTML = `${time} 🇮🇳`;
+}
+updateIndiaTime();
+setInterval(updateIndiaTime, 1000);
