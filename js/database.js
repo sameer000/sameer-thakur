5fa7027ed32536cd704bed45a9080512ad31957e
sameer <thakursameer5893@gmail.com>
2026-03-12 02:20:20 +0530

    Update database.js

--- js/database.js
+++ js/database.js
@@ -40,7 +40,7 @@ async function fetchProjects() {
 function renderProjects(projects) {
   projectsSection.innerHTML = projects.map(project => `
    
-<div class="comic-panel project-card visible">
+<div class="comic-panel project-card">
                         <div class="project-img">
                         <div class="halftone"></div>
                         <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" data-lucide="activity" aria-hidden="true" class="lucide lucide-activity"><path d="M22 12h-2.48a2 2 0 0 0-1.93 1.46l-2.35 8.36a.25.25 0 0 1-.48 0L9.24 2.18a.25.25 0 0 0-.48 0l-2.35 8.36A2 2 0 0 1 4.49 12H2"></path></svg>
@@ -58,6 +58,7 @@ function renderProjects(projects) {
 
 // Call the function to fetch projects
 fetchProjects()
+
 
 
 
